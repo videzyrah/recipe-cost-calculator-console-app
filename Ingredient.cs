c879d5a1c92c6bf5d77d905ce5f9310a4eef8c26
Greg Stull <videzyrah@gmail.com>
2023-07-14 22:14:57 -0400

    added grilled cheese recipe

--- Ingredient.cs
+++ Ingredient.cs
@@ -2,12 +2,14 @@ namespace PocoConsoleApp
 {
     public class Ingredient
     {
+        public string ingredient_name;
         public double unit_price;
         public string conversion_type;
         public double conversion_factor;
         
-        public Ingredient(double Unit_price, string Conversion_type, double Conversion_factor)
+        public Ingredient(string name, double Unit_price, string Conversion_type, double Conversion_factor)
         {
+          ingredient_name = name;
           unit_price = Unit_price;
           conversion_type = Conversion_type;
           conversion_factor = Conversion_factor;
--- Program.cs
+++ Program.cs
@@ -8,15 +8,18 @@
     {
      MainProgram program= new MainProgram();
 
-     Ingredient sandwichBun = new Ingredient(0.6, "per bun", 0.5);
-     Ingredient cheeseSlice = new Ingredient(0.7, "per slice", 0.5);
-     Ingredient beefPatty = new Ingredient(0.8, "per patty", 0.5);
-     Ingredient tomatoSlice = new Ingredient(0.25, "per pound", 0.5);
+     Ingredient sandwichBun = new Ingredient("Sandwich Bun", 0.6, "per bun", 0.5);
+     Ingredient cheeseSlice = new Ingredient("Cheese Slice", 0.7, "per slice", 0.5);
+     Ingredient beefPatty = new Ingredient("BeefPatty", 0.8, "per patty", 0.5);
+     Ingredient tomatoSlice = new Ingredient("Tomato Slice", 0.25, "per pound", 0.5);
+     Ingredient blank = new Ingredient("Nothing", 0, "blank", 0);
 
-     Recipe hamburger = new Recipe(sandwichBun, cheeseSlice, beefPatty, tomatoSlice);
      
-      //Console.WriteLine(sandwichBun.conversion_type);
-      Console.WriteLine("Today's hamburger cost is: $ " +  hamburger.recipe_cost);
+     Recipe hamburger = new Recipe(sandwichBun, cheeseSlice, beefPatty, tomatoSlice);
+     Recipe grilledCheese= new Recipe(sandwichBun, cheeseSlice, blank, blank);
+      
+     Console.WriteLine("Today's hamburger cost is: $ " +  hamburger.recipe_cost);
+     Console.WriteLine("Today's grilled cheese cost is: $ " +  grilledCheese.recipe_cost);
       
     }
   }
